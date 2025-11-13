ea8d961741eca61de39708bd38dec44a306f6d58
Dillon Redding <dillonredding@gmail.com>
2025-11-13 08:46:17 -0600

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Dillon Redding on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRollerTests/DiceRollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerTests.swift
+//  DiceRollerTests
+//
+//  Created by Dillon Redding on 11/13/25.
+//
+
+import Testing
+@testable import DiceRoller
+
+struct DiceRollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
